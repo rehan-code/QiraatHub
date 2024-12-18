6685169b1afb220d87958d637306b03c76f15ad3
Rehan Nagoor <rehannags@yahoo.com>
2024-12-18 16:51:24 +0400

    added footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
+import Footer from "./components/footer";
 
 export const metadata: Metadata = {
   title: "Qiraat Hub",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="">
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import ExpandingGrid from "./client-components/expanding-grid";
-
 export default function Home() {
   return (
     <div>
