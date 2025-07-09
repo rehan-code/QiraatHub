5e90d3c1896deb8836440a3f4ef19d60745e7205
Rehan Nagoor <rehannags@yahoo.com>
2025-07-09 20:49:54 +0400

    added new mushaf; removed mushaf files

--- app/resources/downloads/data/books.ts
+++ app/resources/downloads/data/books.ts
@@ -10,6 +10,32 @@ export interface Book {
 
 export const books: Book[] = [
   {
+    slug: 'mushaf-ibn-dhakwan-an-ibn-amir',
+    title: 'Mushaf Ibn Dhakwan an Ibn Amir',
+    author: 'Ibn Dhakwan an Ibn Amir',
+    coverImage: '/books/Mushaf Ibn Dhakwan an Ibn Amir Cover.png',
+    downloadUrl: '/books/Mushaf Ibn Dhakwan an Ibn Amir.pdf',
+    category: 'Quran',
+  },
+  {
+    slug: 'mushaf-mushaf-qaloon-an-nafi',
+    title: 'Mushaf Mushaf Qaloon an Nafi',
+    author: 'Mushaf Qaloon an Nafi',
+    coverImage: '/books/Mushaf Qaloon an Nafi Cover.png',
+    downloadUrl: '/books/Mushaf Qaloon an Nafi.pdf',
+    description: 'Following Qalun\'s default method of Qasr (shortening) munfasil and sukoon (iskaan) meem al jam',
+    category: 'Quran',
+  },
+  {
+    slug: 'mushaf-warsh-an-nafi-azraq',
+    title: 'Mushaf Warsh An Nafi Azraq',
+    author: 'Nafi Azraq',
+    coverImage: '/books/Mushaf Warsh An Nafi Azraq Cover.png',
+    downloadUrl: '/books/Mushaf Warsh An Nafi Azraq.pdf',
+    description: 'Following the sub-riwayah or "Tariq" Al Azraq, one of Warsh\'s two main transmission paths alongside Al-Asbahani',
+    category: 'Quran',
+  },
+  {
     slug: 'mushaf-warsh-an-nafi-asbahani',
     title: 'Mushaf Warsh An Nafi Asbahani',
     author: 'Nafi Asbahani',
